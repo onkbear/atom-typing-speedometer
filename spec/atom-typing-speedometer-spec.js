de1fca5b712bf687376cdcacd6f5a6204fa87563
onkbear <onkbear@gmail.com>
2017-06-20 20:53:31 +0900

    :green_heart: Fixed spec

--- spec/atom-typing-speedometer-spec.js
+++ spec/atom-typing-speedometer-spec.js
@@ -3,34 +3,37 @@
 describe('AtomTypingSpeedometer', () => {
   let workspaceElement
   let activationPromise
+  let originalConfig
 
   beforeEach(() => {
+    originalConfig = atom.config.get('atom-typing-speedometer')
+    atom.config.set('atom-typing-speedometer.autoToggle', true)
+
     workspaceElement = atom.views.getView(atom.workspace)
     jasmine.attachToDOM(workspaceElement)
 
-    waitsForPromise(() => atom.packages.activatePackage('status-bar'))
-    activationPromise = atom.packages.activatePackage('atom-typing-speedometer')
     waitsForPromise(() => atom.workspace.open())
+    waitsForPromise(() => atom.packages.activatePackage('status-bar'))
+    waitsForPromise(() => atom.packages.activatePackage('atom-typing-speedometer'))
+  })
+
+  afterEach(() => {
+    atom.config.set('atom-typing-speedometer', originalConfig)
   })
 
   describe('when the atom-typing-speedometer:toggle event is triggered', () => {
     it('hides and shows the view', () => {
       // This test shows you an integration test testing at the view level.
-
-      expect(workspaceElement.querySelector('.atom-typing-speedometer')).not.toExist()
+      expect(workspaceElement.querySelector('.atom-typing-speedometer')).toExist()
 
       // This is an activation event, triggering it causes the package to be
       // activated.
       atom.commands.dispatch(workspaceElement, 'atom-typing-speedometer:toggle')
 
-      waitsForPromise(() => {
-        return activationPromise
-      })
-
       runs(() => {
         // Now we can test for view visibility
         let atomTypingSpeedometerElement = workspaceElement.querySelector('.atom-typing-speedometer')
-        expect(atomTypingSpeedometerElement).toBeVisible()
+        expect(atomTypingSpeedometerElement).not.toBeVisible()
 
         atom.commands.dispatch(workspaceElement, 'atom-typing-speedometer:toggle')
         expect(atomTypingSpeedometerElement).not.toBeVisible()
